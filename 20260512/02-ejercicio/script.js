d625a4b8a74b48a1519866f0b0a9cb857d30df65
ivan martin de la rubia <ivanmartindelarubia@gmail.com>
2026-06-01 20:30:03 +0200

    actualizar

---
+++ 20260512/02-ejercicio/script.js
@@ -0,0 +1,7 @@
+const carrito =[
+    {nombre: "🎂 tarta", precio:1.20 },
+    {nombre: "🍪 galleta", precio:0.80 },
+    {nombre: "🍩 donut", precio:1.50 },
+    {nombre: "🍫 chocolate", precio:2.00 },
+    
+]
